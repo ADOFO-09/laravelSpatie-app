ede3fd51965fdc7f32078afd2a122ccbddd2090c
ADOFO-09 <emmanueladofo011@gmail.com>
2024-11-01 13:03:57 +0000

    Views Setup

---
+++ resources/views/role-permission/permission/index.blade.php
@@ -0,0 +1,20 @@
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Permissions
+                            <a href="{{ url('permissions/create')}}" class="btn btn-primary float-end">Add Permission</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+
